9343441bddd32d07eff1333923706eb960096b7b
SakshiReddy <43819508+SakshiReddy@users.noreply.github.com>
2023-04-05 00:04:37 -0400

    DNSServer/

---
+++ dnsPacketModifier.py
@@ -0,0 +1,29 @@
+import socket, time
+from dnsPacket import DNSPacket
+
+class DNSPacketModifier:
+
+    def __init__(self, _serverName, _DNS_UDP_PORT, _BUFFERSIZE):
+        self.DNS_UDP_PORT = _DNS_UDP_PORT
+        self.BUFFERSIZE = _BUFFERSIZE
+        self.serverName = _serverName
+        self.socket_DNS_out = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.dnsCache = {}
+
+    def modify(self, dnsPacket: DNSPacket):
+
+        t = time.time()
+        a = dnsPacket.ArrayOfQuestions[0].get_QNAME()
+
+        if a in self.dnsCache:
+            print(self.dnsCache)
+            dnsPacket.replaceAllAnswers(self.dnsCache[a][0])
+            return dnsPacket
+
+
+        self.socket_DNS_out.sendto(dnsPacket.serializePacket(),(self.serverName, self.DNS_UDP_PORT))
+        data = self.socket_DNS_out.recv(self.BUFFERSIZE)
+        packet = DNSPacket(data)
+        self.dnsCache[a] = (packet.ArrayOfAnswers, time.time())
+
+        return packet
